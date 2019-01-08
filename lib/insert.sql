9127d01f27dce7a7d282932a5bee400ab13dd13c
Elise Phan <aliceconnic@gmail.com>
2019-01-08 23:14:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr.Chocolate",13,"M", "Brown", "sweet", "alive")
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2,"Rowdy",10,"M", "Grey", "grumpy", "alive")
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,"Tabitha",7,"F", "Black", "nice", "alive")
+
